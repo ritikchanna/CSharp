a8b713ba0d8ed860dc00788a62c1d33f0469159f
Ritik Channa <chnritik@gmail.com>
2019-05-14 11:10:54 +0530

    Intial Commit

---
+++ Pallindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pallindrome
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input String to be checked: ");
+            string line = Console.ReadLine();
+            bool isPalindrome = IsPalindrome(line);
+            if (isPalindrome == true)
+                Console.WriteLine(line + " is Palindrome");
+            else
+                Console.WriteLine(line + " is not Palindrome");
+
+            Console.ReadKey();
+        }
+        public static bool IsPalindrome(string line)
+        {
+            if (line.Length <= 1)
+                return true;
+            else
+            {
+                if (line[0] != line[line.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome(line.Substring(1, line.Length - 2));
+            }
+        }
+    }
+}
---
+++ Prime/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Prime
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Input a postive number");
+            int num = Convert.ToInt32(Console.ReadLine());
+           bool isPrime = checkPrime(num,num/2);
+            if (isPrime)
+                Console.Write(num+" is Prime");
+            else
+                Console.WriteLine(num+" is not Prime");
+            Console.ReadKey();
+        }
+
+        static bool checkPrime(int num, int x)
+        {
+            if (x == 1)
+            {
+                return true;
+            }
+            else
+            {
+                if (num%x == 0)
+                    return false;
+                else
+                    return checkPrime(num,x-1);
+            }
+        }
+    }
+}
