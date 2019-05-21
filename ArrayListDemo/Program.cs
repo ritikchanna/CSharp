6b3fd5045a9549ff1493755f3073828a1235b622
Ritik Channa <chnritik@gmail.com>
2019-05-21 15:58:54 +0530

    More C# programs

---
+++ ArrayListDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ArrayListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            bool temp;
+
+            char exit = 'n';
+            do
+            {     
+                Console.WriteLine("Enter element value");
+                list.Add(Console.ReadLine());
+                Console.WriteLine("Enter element(y/n)");
+                temp = char.TryParse(Console.ReadLine(), out exit);
+
+            } while (exit == 'y' || exit == 'Y');
+            Console.WriteLine("Enter element to be searched: ");
+            String check = Console.ReadLine();
+            check = check.Trim();
+            if (list.Contains(check))
+                Console.WriteLine("Element Found at " + list.LastIndexOf(check));
+            else
+                Console.WriteLine("Element Not Found");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GenricsSorting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricsSorting
+{
+    public class Sorting<T1>
+    {   //hashtable
+        //this code
+
+        //T data;
+        T1[] sort(T1[] arary)
+        {   
+            return arary;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
